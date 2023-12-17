e45e276a090772a7176c892fa1a57c8a220932db
Aakanshi <56083_aakanshisharma@gbpuat-tech.ac.in>
2023-12-17 23:30:57 +0530

    added functionality

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
 import streamlit as st
-# import pandas as pd
-# import google.generativeai as genai
 from langchain.llms import CTransformers
 from langchain.prompts import PromptTemplate
 
@@ -9,21 +7,13 @@ from langchain.prompts import PromptTemplate
 def get_response(text, words, style):
     llm = CTransformers(model="models/llama-2-7b-chat.ggmlv3.q8_0.bin", model_type="llama",
                         config={"max_new_tokens": 256, "temperature": 0.01})
-    template = f"write a blog for {style} on the topic {text} within {words} words."
-    prompt = PromptTemplate.from_template("write a blog for {style} on the topic {text} within {words} words.")
-    response = llm(prompt.format(blog_style=style, input_text=text, no_of_words=words))
-    print(words)
+    prompt = PromptTemplate.from_template(f"""write a blog for {style} on the topic {text} within {words} words.""")
+    response = llm(prompt.format())
     print(response)
     return response
 
 
-# GOOGLE_API_KEY=pd.read_csv("key.txt")
-# genai.configure(api_key=GOOGLE_API_KEY)
-#
-# model = genai.GenerativeModel('gemini-pro')
-# response = model.generate_content("What is the meaning of life?")
-# print(response.text)
-# UI
+# -----------------------UI-------------------
 st.set_page_config(page_title='Blog Generation', page_icon="🤖", layout="centered", initial_sidebar_state="collapsed")
 
 st.header("Generate Blogs 🤖")
@@ -39,7 +29,5 @@ with col2:
 submit_button = st.button("Generate")
 
 if submit_button:
-    st.text(input_text)
-    st.text(no_of_words)
-    st.text(blog_style)
-    st.text(get_response(input_text, no_of_words, blog_style))
+    answer = get_response(input_text, no_of_words, blog_style)
+    st.text(answer)
