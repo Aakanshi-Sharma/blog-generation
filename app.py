6c138318770014d61b07158b48a4ff43cfc2dc38
Aakanshi <56083_aakanshisharma@gbpuat-tech.ac.in>
2023-12-19 00:43:54 +0530

    functions changed

--- app.py
+++ app.py
@@ -1,16 +1,31 @@
 import streamlit as st
 from langchain.llms import CTransformers
 from langchain.prompts import PromptTemplate
+import google.generativeai as genai
+
+# -------------GLOBAL VARIABLES--------
+
+with open("key.txt", "r") as f:
+    GOOGLE_API_KEY = f.read()
+
+genai.configure(api_key=GOOGLE_API_KEY)
 
 
 # ------------FUNCTIONS---------------
-def get_response(text, words, style):
-    llm = CTransformers(model="models/llama-2-7b-chat.ggmlv3.q8_0.bin", model_type="llama",
-                        config={"max_new_tokens": 256, "temperature": 0.01})
-    prompt = PromptTemplate.from_template(f"""write a blog for {style} on the topic {text} within {words} words.""")
-    response = llm(prompt.format())
-    print(response)
-    return response
+def get_response_genai(text, words, style):
+    model = genai.GenerativeModel('gemini-pro')
+    response = model.generate_content(f"write a blog for {style} on the topic {text} within {words} words.")
+    print(response.text)
+    return response.text
+
+
+# def get_response(text, words, style):
+#     llm = CTransformers(model="models/llama-2-7b-chat.ggmlv3.q8_0.bin", model_type="llama",
+#                         config={"max_new_tokens": 256, "temperature": 0.01})
+#     prompt = PromptTemplate.from_template(f"""write a blog for {style} on the topic {text} within {words} words.""")
+#     response = llm(prompt.format())
+#     print(response)
+#     return response
 
 
 # -----------------------UI-------------------
@@ -29,5 +44,4 @@ with col2:
 submit_button = st.button("Generate")
 
 if submit_button:
-    answer = get_response(input_text, no_of_words, blog_style)
-    st.write(answer)
+    st.write(get_response_genai(input_text, no_of_words, blog_style))
